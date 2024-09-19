e74c409eeff79610c2bf179c4534568ccbee2405
chojinseok <wlstjr1123@naver.com>
2024-09-19 17:37:32 +0900

    18. 데이터베이스 연동

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -6,10 +6,14 @@ import 'package:flutter/services.dart';
 class CustomTextField extends StatelessWidget {
   final String label;
   final bool isTime;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
 
   const CustomTextField({
     required this.label,
     required this.isTime,
+    required this.onSaved,
+    required this.validator,
     Key? key,
   }) : super(key: key);
 
@@ -28,6 +32,8 @@ class CustomTextField extends StatelessWidget {
         Expanded(
           flex: isTime ? 0 : 1,
           child: TextFormField(
+            onSaved: onSaved,
+            validator: validator,
             cursorColor: Colors.grey,
             maxLines: isTime ? 1 : null,
             expands: !isTime,
